8c11d596a5df947558b476d56b1867902a559fc1
nguyencuong21520 <nguyencuong21520@gmail.com>
2026-01-13 22:14:01 +0700

    feat: add custom styles and display total students in App component

--- TH/lesson3/src/App.jsx
+++ TH/lesson3/src/App.jsx
@@ -3,8 +3,13 @@
 import './App.css'
 import Card from './components/Card'
 function App() {
+
+  //Todo
+  // - using map to render the list of cards
+  // - show total students
   return (
     <>  
+    <h1 className="total-students">Total students: 3</h1>
       <Card name="Nguyễn Văn A" classInfo="CIJS108" description="I am a software engineer" />
       <Card name="Trần Thị B" classInfo="CIJS109" description="I am a teacher" />
       <Card name="Lê Văn C" classInfo="CIJS110" description="I am a student" />
